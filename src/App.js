38c496f57ff80498dd231f0f8cddc1c3a59b29ae
ManjushMerugu <merugumanjusha@gmail.com>
2023-10-17 23:36:52 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
+import WordCounter from "./components/Task1";
+function App() {
+  return (
+    <div>
+      <WordCounter/>
+    </div>
+  );
+}
+export default App;
